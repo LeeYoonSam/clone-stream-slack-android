e00e4786bac28907e424582b82c4e8316adf18e1
이윤삼 <yoonsam@backpac.kr>
2022-07-25 09:10:01 +0900

    Added SlackCloneReactionFactory

---
+++ feat-chat/src/main/java/com/ys/slackclone/uichat/chatthread/composables/reactions/SlackCloneReactionFactory.kt
@@ -0,0 +1,54 @@
+package com.ys.slackclone.uichat.chatthread.composables.reactions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.ys.slackclone.uichat.R
+import io.getstream.chat.android.compose.ui.util.ReactionDrawable
+import io.getstream.chat.android.compose.ui.util.ReactionIcon
+import io.getstream.chat.android.compose.ui.util.ReactionIconFactory
+
+class SlackCloneReactionFactory(
+    private val supportedReactions: Map<String, ReactionDrawable> = mapOf(
+        "good" to ReactionDrawable(
+            iconResId = R.drawable.thumbsup,
+            selectedIconResId = R.drawable.thumbsup
+        ),
+        "love" to ReactionDrawable(
+            iconResId = R.drawable.love,
+            selectedIconResId = R.drawable.love
+        ),
+        "smile" to ReactionDrawable(
+            iconResId = R.drawable.smile,
+            selectedIconResId = R.drawable.smile
+        ),
+        "joy" to ReactionDrawable(
+            iconResId = R.drawable.joy,
+            selectedIconResId = R.drawable.joy
+        ),
+        "wink" to ReactionDrawable(
+            iconResId = R.drawable.wink,
+            selectedIconResId = R.drawable.wink
+        )
+    )
+) : ReactionIconFactory {
+
+    @Composable
+    override fun createReactionIcon(type: String): ReactionIcon {
+        val reactionDrawable = requireNotNull(supportedReactions[type])
+        return ReactionIcon(
+            painter = painterResource(id = reactionDrawable.iconResId),
+            selectedPainter = painterResource(id = reactionDrawable.selectedIconResId)
+        )
+    }
+
+    @Composable
+    override fun createReactionIcons(): Map<String, ReactionIcon> {
+        return supportedReactions.mapValues {
+            createReactionIcon(it.key)
+        }
+    }
+
+    override fun isReactionSupported(type: String): Boolean {
+        return supportedReactions.containsKey(type)
+    }
+}
